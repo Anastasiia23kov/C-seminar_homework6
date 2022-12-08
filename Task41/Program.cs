1b61cb0ae6bffb13577aea5664d0665453150db6
Anastasiia <penkova23anastasia@gmail.com>
2022-12-08 21:45:47 +0200

    Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.

---
+++ Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M]; 
+for(int i=0; i<array.Length; i++)
+{
+    Console.Write($"Введите элемент {i+1} массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Начальный массив:[{string.Join(", ", array)}]");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+count++;
+}
+Console.WriteLine($"Кол-во положительных чисел:{count}");
